7d6bfd9baedf900b36526a3155d13116e1864855
yuma nacamura <yuma.nacamura@gmail.com>
2018-09-25 13:15:07 +0900

    remake for haiku

---
+++ fmtdata.py
@@ -0,0 +1,57 @@
+import pickle
+import MeCab
+import re
+from collections import Counter
+import numpy as np
+
+th_count =1
+words = []
+shkigo = []
+m = MeCab.Tagger('-Owakati')
+p = re.compile(r'[（(].+?[）)]')
+
+with open('data/hkigo.pickle','rb') as f:
+    hkigo = pickle.load(f)
+
+#parse haiku
+for h,k in hkigo:
+    rm = p.findall(h)
+    for r in rm:
+        h = h.replace(r,'')
+    sh = m.parse(h).split()
+    if len(sh)>19:
+        print('yabai'+'#'*300,h)
+        input()
+        continue
+    if k not in sh:
+        print(k,'isnt in',sh)
+        continue
+    words+=sh
+    shkigo.append((sh,k))
+
+#remove word
+words = [w for w,c in Counter(words).items() if c>=th_count]
+
+#make index
+index = ['<EOS>','<p>']+words
+index_inv = {w:i for i,w in enumerate(index)}
+
+
+ihaiku = []
+ikigo = []
+
+#word2index
+for sh,k in shkigo:
+    for w in sh:
+        if w not in words:
+            print(w,'is too little')
+            continue
+        ih = [index_inv[w] for w in sh]
+        ihaiku.append(ih + [0] + [1 for i in range(19-len(ih))])
+        ikigo.append(index_inv[k])
+with open('data/ihaiku_ikigo.pickle','wb') as f:
+    pickle.dump((np.array(ihaiku),np.array(ikigo)),f)
+with open('data/index.pickle','wb') as f:
+    pickle.dump(index,f)
+with open('data/index_inv.pickle','wb') as f:
+    pickle.dump(index_inv,f)
---
+++ makedata.py
@@ -0,0 +1,24 @@
+import re
+import pickle
+import urllib.request
+import urllib.error
+from time import sleep
+url = "http://www.haiku-data.jp/work_detail.php?cd="
+
+ph = re.compile(r'(?<="><B>).+(?=&nbsp</B)')
+pk = re.compile(r'(?<=季　語</font></div></td> \n                  <td width="76%" background="img/bg-w.gif" bgcolor="#FFFFFF"> <div align="center" class="font2">).+(?=&nbsp</div></td> \n                </tr> \n                <tr> \n                  <td background="img/bg6.gif"><div align="center"><span class="fontff"><font color="#FFFFFF">季　節)')
+
+hkigo = []
+
+for i in range(1,41189):
+    sleep(1)
+    resource = urllib.request.urlopen(url+str(i))
+    contents = resource.read().decode()
+    haiku = ph.findall(contents)
+    kigo = pk.findall(contents)
+    if not haiku or not kigo:
+        print('empty at',i)
+        continue
+    hkigo.append(haiku+kigo)
+with open('data/hkigo.pickle','wb') as f:
+    pickle.dump(hkigo,f)
